a121b87b5a3ae029f5d2b661e7c9c5cf104c1668
Matthew <matthew.j.erwin@gmail.com>
2016-02-17 21:50:20 +0000

    Adds first unit test

---
+++ tests/ParserTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MJErwin\ParseAChangelog\Tests;
+
+use MJErwin\ParseAChangelog\Reader;
+
+/**
+ * @author Matthew Erwin <m@tthewerwin.com>
+ * www.matthewerwin.co.uk
+ */
+class ParserTest extends \PHPUnit_Framework_TestCase
+{
+    public function testReleaseCount()
+    {
+        $changelog = new Reader('data/changelog_1.md');
+        $releases = $changelog->getReleases();
+
+        $this->assertEquals(12, sizeof($releases));
+    }
+}
