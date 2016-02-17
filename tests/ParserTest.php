d5db5213575888e1654409d7372410de588bb5f1
Matthew <matthew.j.erwin@gmail.com>
2016-02-17 22:59:18 +0000

    Adds getRelease test

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -17,4 +17,12 @@ class ParserTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals(12, sizeof($releases));
     }
+
+    public function testGetRelease()
+    {
+        $changelog = new Reader(__DIR__ . '/data/changelog_1.md');
+        $release = $changelog->getRelease('0.0.8');
+
+        $this->assertEquals('0.0.8', $release->getVersion());
+    }
 }
