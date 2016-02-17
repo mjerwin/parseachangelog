1cb65f28e57ad197e734af5b3a90b7fe0d1b1d97
Matthew <matthew.j.erwin@gmail.com>
2016-02-17 22:51:45 +0000

    Implements testReleaseVersion()

--- src/Release.php
+++ src/Release.php
@@ -9,6 +9,8 @@ namespace MJErwin\ParseAChangelog;
 class Release
 {
     protected $content;
+    protected $version;
+    protected $date;
 
     /**
      * Release constructor.
@@ -18,5 +20,20 @@ class Release
     public function __construct($content)
     {
         $this->content = $content;
+
+        $heading_line = isset($this->content[0]) ? $this->content[0] : null;
+
+        if($heading_line)
+        {
+            preg_match('/^## (\[?)(?<version>[^\s\[\]#]*)(\]?)( - (?<date>[0-9]{4}-[0-9]{2}-[0-9]{2}))?$/', $heading_line, $matches);
+
+            $this->version = isset($matches['version']) ? $matches['version'] : null;
+            $this->date = isset($matches['date']) ? $matches['date'] : null;
+        }
+    }
+
+    public function getVersion()
+    {
+        return $this->version;
     }
 }
