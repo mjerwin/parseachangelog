0bb532845edf9832eb27989d63f50240fc0376f2
Matthew <matthew.j.erwin@gmail.com>
2016-02-23 20:08:46 +0000

    Adds JSON test

--- tests/ReleaseTest.php
+++ tests/ReleaseTest.php
@@ -59,6 +59,17 @@ notable changes.',
         $this->assertEquals($expected, $release->getRemoved());
     }
 
+    public function testJson()
+    {
+        $data = $this->loadContent('release_content_2.md');
+
+        $release = new Release($data);
+
+        $expected = '{"version":"0.0.4","date":"2014-08-09","added":["Better explanation of the difference between the file (\"CHANGELOG\")\nand its function \"the change log\"."],"changed":["Refer to a \"change log\" instead of a \"CHANGELOG\" throughout the site\nto differentiate between the file and the purpose of the file \u2014 the\nlogging of changes."],"removed":["Remove empty sections from CHANGELOG, they occupy too much space and\ncreate too much noise in the file. People will have to assume that the\nmissing sections were intentionally left out because they contained no\nnotable changes."]}';
+
+        $this->assertEquals($expected, $release->toJson());
+    }
+
     public function loadContent($filename)
     {
         return file(__DIR__ . '/data/' . $filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
